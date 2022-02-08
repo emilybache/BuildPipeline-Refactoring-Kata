72441517debef01aaa0a591a0361963b4f2568ba
emily <emily@bacheconsulting.com>
2022-02-08 10:12:06 +0100

    rename to match new name of kata

--- csharp/BuildPipelineTests/CapturingLogger.cs
+++ csharp/BuildPipelineTests/CapturingLogger.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UntangledConditionals;
 
-namespace TestProject1
+namespace BuildPipelineTests
 {
     internal class CapturingLogger : Logger
     {
