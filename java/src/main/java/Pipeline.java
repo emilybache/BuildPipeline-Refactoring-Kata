a8655b1c7a042fa0cf4e0659c505f8ae6c3771e1
emily <emily@bacheconsulting.com>
2025-12-17 14:31:14 +0100

    split creation of pipeline from running it

--- java/src/main/java/Pipeline.java
+++ java/src/main/java/Pipeline.java
@@ -3,29 +3,30 @@ import dependencies.Emailer;
 import dependencies.Logger;
 import dependencies.Project;
 
+import java.util.List;
+
 public class Pipeline {
     private final Config config;
     private final Emailer emailer;
     private final Logger log;
-
-
+    private final List<PipelineStep> pipeline;
 
     public Pipeline(Config config, Emailer emailer, Logger log) {
         this.config = config;
         this.emailer = emailer;
         this.log = log;
-
+        this.pipeline = List.of(
+                new TestStep(config, log),
+                new DeployStep(config, log),
+                new ReportStep(config, log, emailer)
+        );
     }
 
     public void run(Project project) {
-        var emptyStepResult = new PipelineStepResult("", true, "");
-        var testStep = new TestStep(config, log);
-        var deployStep = new DeployStep(config, log);
-        var reportStep = new ReportStep(config, log, emailer);
-
-        var testResult = testStep.run(project, emptyStepResult);
-        var deployResult = deployStep.run(project, testResult);
-        var endResult = reportStep.run(project, deployResult);
+        var previousStepResult = new PipelineStepResult("", true, "");
+        for (var step : pipeline) {
+            previousStepResult = step.run(project, previousStepResult);
+        }
     }
 
 }
